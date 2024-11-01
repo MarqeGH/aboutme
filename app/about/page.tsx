0b9613cf682d93cb3bce7af62f0a450c8bf749a8
MarqeGH <marqe.mywords@gmail.com>
2024-11-01 14:02:47 +0100

    update site, add icon

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function AboutUs() {
+    return (
+    <main>
+        <h1>about us</h1>
+        <Link href="../">Go back</Link>
+    </main>
+    );
+}
