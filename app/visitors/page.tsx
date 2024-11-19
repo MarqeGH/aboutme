e8323fe76b89b184935a40ad889103f6906151e6
MarqeGH <marqe.mywords@gmail.com>
2024-11-19 13:33:00 +0100

    add use client to visitors page

--- app/visitors/page.tsx
+++ app/visitors/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState, useEffect, useRef } from 'react';
 import { signOut, signIn, useSession } from 'next-auth/react';
 import { createComment, getMessages } from '../actions';
