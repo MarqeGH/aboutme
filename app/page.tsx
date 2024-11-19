3d2c3e22fc11265c02edbbf186dfdaad6e5b1a57
MarqeGH <marqe.mywords@gmail.com>
2024-11-19 13:43:40 +0100

    pass on status from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 import Link from 'next/link';
 import Header from '@/components/header';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { status } = useSession();
 
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
-
 
   return (
     <div className="p-8">
